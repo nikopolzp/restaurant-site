ed26090c20814aa1a686ba0a8a403a5247a1523a
nikopolzp <111646522+nikopolzp@users.noreply.github.com>
2022-12-18 21:57:18 +0000

    Deploying to gh-pages from @ nikopolzp/restaurant-site@d7677c3e576669af3ea125c87a5b1bcc8c66670e 🚀

---
+++ triple-room.81a35e6d.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{speed:2500,loop:!0,autoplay:{delay:2500,disableOnInteraction:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=triple-room.81a35e6d.js.map
---
+++ triple-room.c5f9f780.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{speed:2500,loop:!0,autoplay:{delay:2500,disableOnInteraction:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=triple-room.c5f9f780.js.map
