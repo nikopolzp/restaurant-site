96d48b4546ea08508de034c33cb22a501d557c35
nikopolzp <111646522+nikopolzp@users.noreply.github.com>
2022-12-23 22:25:21 +0000

    Deploying to gh-pages from @ nikopolzp/restaurant-site@528b47cce53b4ea3592a3e6204ce119c0c92a091 🚀

---
+++ index.39cd9e6e.js
@@ -0,0 +1,2 @@
+function scrollFunction(){var o=document.getElementById("header");document.body.scrollTop>100||document.documentElement.scrollTop>100?o.classList.add("active"):o.classList.remove("active")}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.39cd9e6e.js.map
