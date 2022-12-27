56b539fa903b7ea5cd8948ec614d47837e216015
Ruslan P <nikopolzp@gmail.com>
2022-12-27 15:54:27 +0200

    add select room in price-room modal and add js calculator

---
+++ src/js/calculator-price.js
@@ -0,0 +1,20 @@
+const numberCastInput = document.querySelector('[name=numberCast]');
+const numberCostInput = document.querySelector('[name=numberCost]');
+const total = document.querySelector('.total');
+const quantityLabel = document.querySelector('.quantity-label');
+
+function calculateCost() {
+  const numberCast = numberCastInput.value;
+  const numberCost = numberCostInput.value;
+  const cost = numberCast * numberCost;
+
+  console.log(numberCast);
+  console.log(numberCost);
+  console.log(cost);
+  document.getElementById('numberСost').value = cost.toFixed(2);
+}
+
+calculateCost();
+
+numberCastInput.addEventListener('input', calculateCost);
+numberCostInput.addEventListener('input', calculateCost);
